828894296203d3f668faf7071356761f1302328e
AnkitKumar <ankit17110747@yahoo.com>
2024-08-02 15:11:10 +0530

    added more services in flight

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,6 +1,6 @@
 const CrudRepository = require('./crud-repository');
-const { Flight, Airplane, Airport, City} = require('../models')
-const {Op, Sequelize, col} = require('sequelize');
+const { Flight, Airplane, Airport, City } = require('../models')
+const { Op, Sequelize, col } = require('sequelize');
 const { on } = require('nodemon');
 
 class FlightRepository extends CrudRepository {
@@ -8,7 +8,7 @@ class FlightRepository extends CrudRepository {
         super(Flight)
     }
 
-    async getAllFlights(data, sort){
+    async getAllFlights(data, sort) {
         const response = await Flight.findAll({
             where: data,
             order: sort,
@@ -23,10 +23,10 @@ class FlightRepository extends CrudRepository {
                     required: true,
                     as: 'departureAirport',
                     on: {           // on the specific column
-                        col1: Sequelize.where(Sequelize.col('Flight.departureAirportId'), "=" , (Sequelize.col('departureAirport.code')))
+                        col1: Sequelize.where(Sequelize.col('Flight.departureAirportId'), "=", (Sequelize.col('departureAirport.code')))
                     },
                     include: {
-                        model:City,
+                        model: City,
                         require: true,
                         as: 'cityDetail',
                     }
@@ -36,10 +36,10 @@ class FlightRepository extends CrudRepository {
                     required: true,
                     as: 'arrivalAirport',
                     on: {           // on the specific column
-                        col1: Sequelize.where(Sequelize.col('Flight.departureAirportId'), "=" , (Sequelize.col('arrivalAirport.code')))
+                        col1: Sequelize.where(Sequelize.col('Flight.departureAirportId'), "=", (Sequelize.col('arrivalAirport.code')))
                     },
                     include: {
-                        model:City,
+                        model: City,
                         require: true,
                         as: 'cityDetail',
                     }
@@ -47,6 +47,19 @@ class FlightRepository extends CrudRepository {
             ]
         })
         return response;
+    }
+
+    async updateRemainingSeats(flightId, seats, dec = true) {
+        console.log(flightId, seats, dec, 'print all');
+        
+        const flight = await Flight.findByPk(flightId);
+        if (parseInt(dec)) {
+            await flight.decrement('totalSeats', { by: seats, returning: true });
+        } else {
+            await flight.increment('totalSeats', { by: seats, returning: true });
+        }
+        await flight.reload();
+        return flight;
     }
 }
 
--- src/services/flightService.js
+++ src/services/flightService.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
-const {FlightRepository} = require('../repositories'); 
+const { FlightRepository } = require('../repositories');
 const { AppError } = require('../utils/error/index');
 const { Op, fn, col, where } = require('sequelize');
 const moment = require('moment');
@@ -26,28 +26,28 @@ async function getAllFlights(query) {
     let customFilter = {};
     let sortFilter = [];
     let endTime = " 23:59:00"
-    if(query.trips) {
-       [departureAirportId, arrivalAirportId]  = query.trips.split("-");
-       customFilter.departureAirportId = departureAirportId;
-       customFilter.arrivalAirportId = arrivalAirportId;
+    if (query.trips) {
+        [departureAirportId, arrivalAirportId] = query.trips.split("-");
+        customFilter.departureAirportId = departureAirportId;
+        customFilter.arrivalAirportId = arrivalAirportId;
     }
-    if(query.price) {
+    if (query.price) {
         [minPrice, maxPrice] = query.price.split('-');
         customFilter.price = {
-            [Op.between] : [minPrice, ((maxPrice === undefined )? 20000: maxPrice)]
+            [Op.between]: [minPrice, ((maxPrice === undefined) ? 20000 : maxPrice)]
         }
     }
-    if(query.traveller){
+    if (query.traveller) {
         customFilter.totalSeats = {
-            [Op.gte] : query.traveller,
+            [Op.gte]: query.traveller,
         }
     }
-    if(query.tripDate) {
+    if (query.tripDate) {
         customFilter.departureTime = {
             [Op.between]: [query.tripDate, query.tripDate + endTime]
         }
     }
-    if(query.sort) {
+    if (query.sort) {
         const params = query.sort.split(',');
         const sortFilters = params.map((p) => p.split('_'));
         sortFilter = sortFilters;
@@ -61,7 +61,29 @@ async function getAllFlights(query) {
     }
 }
 
+async function getFlight(id) {
+    try {
+        const flight = await flightRepository.get(id);
+        return flight;
+    } catch (error) {
+        if(error.statusCode === StatusCodes.NOT_FOUND){
+            throw new AppError('The request Flight not found', error.statusCode);
+        }
+        throw new AppError('Cannot fetch the details of Flight', StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+}
+
+async function updateFlight(flightId, data) {
+    try {
+        const response = await flightRepository.updateRemainingSeats(flightId, data.seats, data.dec);
+        return response;
+    } catch (error) {
+        throw new AppError('Cannot fetch the details of Flight', StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+}
 module.exports = {
     createFlights,
     getAllFlights,
+    getFlight,
+    updateFlight
 }
